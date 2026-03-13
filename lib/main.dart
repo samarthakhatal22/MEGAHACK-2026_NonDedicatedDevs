6a62feb9e931b7422c845357733e9a84f64ed1f8
Samartha Khatal <samartha.2403434206@vcet.edu.in>
2026-03-13 15:10:05 +0530

    Added homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:civicshield/pages/authenticate.dart';
+import 'package:civicshield/pages/home_page.dart';
+
 void main() {
-  runApp(const MainApp());
+  runApp(const CivicShieldApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class CivicShieldApp extends StatelessWidget {
+  const CivicShieldApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'PolicyLens',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF6750A4),
+          brightness: Brightness.light,
         ),
+        fontFamily: 'GoogleSans',
       ),
+      home: const HomePage(),
     );
   }
 }
